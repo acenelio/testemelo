1d10ce1cbef6fba9e59a78660d627646d60fbc1f
Robson Melo Souza <melodisouza@gmail.com>
2021-05-04 09:17:12 -0300

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className= "text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className= "text-primary">Olá Mundo!</h1>
+      
+        <DataTable />
+      </div>
+      
+      <Footer />
+    </>
   );
 }
 
